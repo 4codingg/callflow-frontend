15fcfb4ae0c0d8156a7c3e90f7f29009b84f42f7
CaioAlmeida09 <Caio09092001@gmail.com>
2024-04-27 13:54:08 -0300

    fix:LabelPhone

--- src/pageTemplates/MembersTemplate/CreateMemberTemplate/index.tsx
+++ src/pageTemplates/MembersTemplate/CreateMemberTemplate/index.tsx
@@ -80,7 +80,7 @@ export const CreateMemberTemplate = () => {
           {...getFieldProps("email")}
         />
         <Input
-          label="Phone"
+          label="Telefone"
           placeholder="Digite a seu telefone"
           {...getFieldProps("phone")}
         />
