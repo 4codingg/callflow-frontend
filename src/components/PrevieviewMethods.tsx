d40c710d450c15e0d79a0d2c624799cbd2bb2d28
CaioAlmeida09 <Caio09092001@gmail.com>
2024-05-20 21:42:08 -0300

    fix-DropdownPaymentMethodAddBalance

--- src/components/PrevieviewMethods.tsx
+++ src/components/PrevieviewMethods.tsx
@@ -1,10 +1,5 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { CaretDown } from "phosphor-react";
-import * as Select from "@radix-ui/react-select";
-import clsx from "clsx";
-import { DropdownItem } from "./DropdownItem";
-import { Label } from "./Label";
 import { Spinner } from "./Spinner";
 
 import HiperCardIcon from "@/assets/icons/hipercard-icon.svg";
@@ -12,6 +7,7 @@ import MasterCardIcon from "@/assets/icons/mastercard-icon.svg";
 import EloIcon from "@/assets/icons/elo-icon.svg";
 import VisaIcon from "@/assets/icons/visa-icon.svg";
 import { IPaymentMethod } from "@/@types/PaymentMethod";
+import { Paragraph } from "./Paragraph";
 
 interface DropdownPaymentMethodsProps {
   label?: string;
@@ -23,12 +19,8 @@ interface DropdownPaymentMethodsProps {
 }
 
 export const PreviewPaymentMethod = ({
-  value,
   paymentMethods,
-  label,
-  placeholder = "Select",
   isLoading = false,
-  className,
 }: DropdownPaymentMethodsProps) => {
   const [defaultCard, setDefaultCard] = useState<IPaymentMethod>();
   useEffect(() => {
@@ -40,47 +32,27 @@ export const PreviewPaymentMethod = ({
 
   console.log(defaultCard);
   return (
-    <Label name={label} className={clsx("w-full", className)}>
-      <Select.Root defaultValue={value} value={value}>
-        <Select.Trigger
-          className={clsx(
-            "p-3 text-sm font-poppins h-[40px] w-full flex justify-between items-center rounded-lg border border-muted shadow-sm"
+    <section>
+      {isLoading ? (
+        <div className="flex flex-1 justify-center items-center">
+          <Spinner />
+        </div>
+      ) : (
+        <div>
+          {defaultCard && (
+            <div
+              className=" flex gap-4 justify-center items-center py-2 border-2"
+              key={defaultCard.id}
+            >
+              <div> {getIconBrand(defaultCard.brand)}</div>
+              <Paragraph>
+                {`**** **** **** ${defaultCard.last4} - ${defaultCard.nickname}`}
+              </Paragraph>
+            </div>
           )}
-          aria-label={label}
-        >
-          <Select.Value placeholder={placeholder} className="font-normal" />
-          <Select.Icon>
-            <CaretDown size={16} />
-          </Select.Icon>
-        </Select.Trigger>
-        <Select.Portal>
-          <Select.Content
-            position="popper"
-            side="bottom"
-            className="bg-white p-4 flex flex-row border border-neutral-grey rounded z-[10000] min-w-[350px] max-h-[300px]"
-          >
-            <Select.Viewport className="text-neutral-darkest font-poppins">
-              <Select.Group>
-                {isLoading ? (
-                  <div className="flex flex-1 justify-center items-center">
-                    <Spinner />
-                  </div>
-                ) : (
-                  defaultCard && (
-                    <DropdownItem
-                      key={defaultCard.id}
-                      leftIcon={getIconBrand(defaultCard.brand)}
-                      label={`**** **** **** ${defaultCard.last4} - ${defaultCard.nickname}`}
-                      value={defaultCard.id.toString()}
-                    />
-                  )
-                )}
-              </Select.Group>
-            </Select.Viewport>
-          </Select.Content>
-        </Select.Portal>
-      </Select.Root>
-    </Label>
+        </div>
+      )}
+    </section>
   );
 };
 
