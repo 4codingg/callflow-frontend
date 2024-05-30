d6ab1283d5e8c74b39162739dda7774c1f0cab6a
João Guilherme <joaoguilherme_pe@hotmail.com>
2024-05-30 18:08:18 -0300

    Fixes

--- src/pageTemplates/MetricsTemplate/ServicesUsageCostChart.tsx
+++ src/pageTemplates/MetricsTemplate/ServicesUsageCostChart.tsx
@@ -13,6 +13,49 @@ import {
 } from "recharts";
 import { CustomTooltip } from "./CustomTooltip";
 
+const data = [
+  {
+    name: "Junho 2023",
+    cost: 135,
+  },
+  {
+    name: "Julho 2023",
+    cost: 222,
+  },
+  {
+    name: "Agosto 2023",
+    cost: 300,
+  },
+  {
+    name: "Setembro 2023",
+    cost: 200,
+  },
+  {
+    name: "Outubro 2023",
+    cost: 278,
+  },
+  {
+    name: "Novembro 2023",
+    cost: 189,
+  },
+  {
+    name: "Dezembro 2023",
+    cost: 239,
+  },
+  {
+    name: "Janeiro 2024",
+    cost: 297,
+  },
+  {
+    name: "Fevereiro 2024",
+    cost: 294,
+  },
+  {
+    name: "Março 2024",
+    cost: 222,
+  },
+];
+
 const options = [
   {
     label: "SMS",
@@ -28,7 +71,7 @@ const options = [
   },
 ];
 
-export const ServicesUsageCostChart = ({ label, data, dataKey, fillColor }) => {
+export const ServicesUsageCostChart = ({}) => {
   const [serviceActive, setServiceActive] = useState(options[0].value);
 
   const handleChange = (value: string) => {
@@ -111,9 +154,9 @@ export const ServicesUsageCostChart = ({ label, data, dataKey, fillColor }) => {
           />
           <Bar
             label="SMSs"
-            dataKey={dataKey}
+            dataKey={"cost"}
             fill="#783EFD"
-            activeBar={<Rectangle fill={fillColor} />}
+            activeBar={<Rectangle fill={"#783EFD"} />}
           />
         </BarChart>
       </ResponsiveContainer>
