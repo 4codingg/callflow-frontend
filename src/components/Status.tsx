e338d48bd749db8d9f039100d100c4622b08616c
João Guilherme <joaoguilherme_pe@hotmail.com>
2024-07-03 20:43:23 -0300

    Feat

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,7 +1,6 @@
 import { getActivitieLabel } from '@/utils/getActivitieLabel';
 import clsx from 'clsx';
 import { Paragraph } from './Paragraph';
-import { Circle } from 'lucide-react';
 
 interface IStatusProps {
   value: 'completed' | 'pending' | 'failed';
@@ -9,22 +8,17 @@ interface IStatusProps {
 
 export const Status = ({ value }: IStatusProps) => {
   return (
-    <div className={clsx('w-[15%] text-right flex items-center gap-2')}>
-      <Circle
-        color="#000"
-        className={clsx('flex w-2 h-2 rounded-full', {
+    <div
+      className={clsx(
+        'text-right flex items-center justify-center w-[120px] gap-2 rounded-full py-2',
+        {
           'bg-green': value === 'completed',
-          'bg-primary': value === 'pending',
+          'bg-medium-grey': value === 'pending',
           'bg-negative-dark': value === 'failed',
-        })}
-      />
-      <Paragraph
-        className={clsx({
-          '!text-green': value === 'completed',
-          '!text-primary': value === 'pending',
-          '!text-negative-dark': value === 'failed',
-        })}
-      >
+        }
+      )}
+    >
+      <Paragraph className={clsx('text-white !font-semibold')}>
         {getActivitieLabel(value)}
       </Paragraph>
     </div>
